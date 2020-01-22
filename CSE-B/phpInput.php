2be5a29fffa2e36b221c24456df171ee4434abaf
vedant1998 <vedantdobhal@gmail.com>
2020-01-22 14:37:39 +0530

    Just added PHPInput

---
+++ CSE-B/phpInput.php
@@ -0,0 +1,27 @@
+<?php  
+if (isset($_POST['btn'])){
+	
+
+echo $_POST['id'];
+echo $_POST['username'];
+echo $_POST['age'];
+echo $_POST['Contact'];
+echo $_POST['emailid'];
+}
+?>
+<html>
+<head>
+	<title> Input Form</title>
+</head>
+<body>
+	<form action="phpInput.php" method="post">
+		ID:<input name="id", type ="text" placeholder="Enter ID"><br>
+ Name:<input type="text" name="username" placeholder="Enter Name">
+  <br>
+ Age:<input type="text" name="age" placeholder="Wnter age"><br>
+ Contact:<input type="text" name="contact" placeholder="Enter Contact"><br>
+ email:<input type="text" name="emailid" placeholder="Enter MailID"><br>
+<input type="submit" name="btn">
+</form>
+</body>
+</html>
